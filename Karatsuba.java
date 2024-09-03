e1c97e6494da0c9f8f026e8c346c8cc654b26f04
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2024-09-03 11:33:20 -0300

    ajustes de formatacao

--- Karatsuba.java
+++ Karatsuba.java
@@ -136,6 +136,10 @@ public class Karatsuba{
     //         sb.append('1');
     //     }
 
+    //     while (sb.length() > 1 && sb.charAt(sb.length() - 1) == '0') {
+    //         sb.deleteCharAt(sb.length() - 1);
+    //     }
+        
     //     return sb.reverse().toString();
     // }
 
